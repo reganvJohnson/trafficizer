b87767baf05b4fa513e426e26060a08635f95be8
Regan Johnson <regan.johnson@points.com>
2018-01-01 10:50:11 -0500

    here we go!

---
+++ src/main/java/com/lorisoft/trafficizer/TrafficizerApplication.java
@@ -0,0 +1,12 @@
+package com.lorisoft.trafficizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficizerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lorisoft/trafficizer/TrafficizerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lorisoft.trafficizer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrafficizerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
